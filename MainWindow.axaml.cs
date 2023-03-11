c8c33da661cbd09d63095188b10ba780f47e377e
Plextora <71889427+Plextora@users.noreply.github.com>
2023-03-11 17:10:37 -0500

    Window is now moveable

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,12 +1,42 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 
-namespace osrUI
+namespace osrUI;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    private bool _isMouseDown;
+    private PointerPoint _originalPoint;
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
+
+    #region Make window moveable
+
+    private void MainPanel_OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+        if (!_isMouseDown) return;
+
+        var currentPoint = e.GetCurrentPoint(this);
+        Position = new PixelPoint(Position.X + (int)(currentPoint.Position.X - _originalPoint.Position.X),
+            Position.Y + (int)(currentPoint.Position.Y - _originalPoint.Position.Y));
+    }
+
+    private void MainPanel_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (WindowState == WindowState.Maximized || WindowState == WindowState.FullScreen) return;
+
+        _isMouseDown = true;
+        _originalPoint = e.GetCurrentPoint(this);
+    }
+
+    private void MainPanel_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        _isMouseDown = false;
+    }
+
+    #endregion
 }
