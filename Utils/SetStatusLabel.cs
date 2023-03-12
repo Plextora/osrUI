a767b39741055d15011c114c78cfe789631bf2f4
Plextora <71889427+Plextora@users.noreply.github.com>
2023-03-12 12:13:13 -0400

    Added SetStatusLabel class

---
+++ Utils/SetStatusLabel.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace osrUI.Utils
+{
+    public class SetStatusLabel
+    {
+        public static void Default(Label? statusLabel)
+        {
+            if (statusLabel == null) return;
+            statusLabel.Foreground = Brushes.White;
+            statusLabel.Content = "Status: Idle";
+        }
+
+        public static void Pending(Label? statusLabel, string statusText)
+        {
+            if (statusLabel == null) return;
+            statusLabel.Foreground = Brushes.Khaki;
+            statusLabel.Content = $"Status: {statusText}";
+        }
+
+        public static void Completed(Label? statusLabel, string statusText)
+        {
+            if (statusLabel == null) return;
+            statusLabel.Foreground = Brushes.LightGreen;
+            statusLabel.Content = $"Status: {statusText}";
+        }
+
+        public static void Error(Label? statusLabel, string statusText)
+        {
+            if (statusLabel == null) return;
+            statusLabel.Foreground = Brushes.Crimson;
+            statusLabel.Content = $"Status: {statusText}";
+        }
+    }
+}
